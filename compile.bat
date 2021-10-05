1fecdfaac7dd19e1aa7e11f76a4d8d5b0e0bd102
piepgras <thepiepgras@gmail.com>
2021-10-05 15:20:26 +0200

    Simple Gravity Test

---
+++ compile.bat
@@ -0,0 +1,2 @@
+C:\GFX\Vulkan\Bin\glslc.exe simple_shader.vert -o simple_shader.vert.spv
+C:\GFX\Vulkan\Bin\glslc.exe simple_shader.frag -o simple_shader.frag.spv
---
+++ simple_render_system.cpp
@@ -0,0 +1,165 @@
+#include "simple_render_system.hpp"
+#include "lve_renderer.hpp"
+
+//libs
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+// std
+#include <array>
+#include <stdexcept>
+
+namespace lve {
+
+    struct SimplePushConstantData {
+        glm::mat2 transform{ 1.f };
+        glm::vec2 offset;
+        alignas(16) glm::vec3 color;
+    };
+
+    SimpleRenderSystem::SimpleRenderSystem(LveDevice& device, VkRenderPass renderPass) : lveDevice{ device } {
+
+        createPipelineLayout();
+        createPipeline(renderPass);
+    }
+
+    SimpleRenderSystem::~SimpleRenderSystem() {
+        vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr);
+    }
+
+    void SimpleRenderSystem::createPipelineLayout() {
+        VkPushConstantRange pushConstantRange{};
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(SimplePushConstantData);
+
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount = 0;
+        pipelineLayoutInfo.pSetLayouts = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
+        if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
+            VK_SUCCESS) {
+            throw std::runtime_error("Failed to create pipeline layout!");
+        }
+    }
+
+    void SimpleRenderSystem::createPipeline(VkRenderPass renderPass) {
+        assert(pipelineLayout != nullptr && "Cannot create pipeline before pipeline layout");
+
+        PipelineConfigInfo pipelineConfig{};
+        LvePipeline::defaultPipelineConfigInfo(pipelineConfig),
+        pipelineConfig.renderPass = renderPass;
+        pipelineConfig.pipelineLayout = pipelineLayout;
+        lvePipeline = std::make_unique<LvePipeline>(
+            lveDevice,
+            "simple_shader.vert.spv",
+            "simple_shader.frag.spv",
+            pipelineConfig);
+    }
+
+    void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<LveGameObject> &gameObjects) {
+        // update
+        int i = 0;
+        for (auto& obj : gameObjects) {
+            i += 1;
+            obj.transform2d.rotation =
+                glm::mod<float>(obj.transform2d.rotation + 0.00001f * i, 2.f * glm::pi<float>());
+        }
+
+        // render
+        lvePipeline->bind(commandBuffer);
+        for (auto& obj : gameObjects) {
+            SimplePushConstantData push{};
+            push.offset = obj.transform2d.translation;
+            push.color = obj.color;
+            push.transform = obj.transform2d.mat2();
+
+            vkCmdPushConstants(
+                commandBuffer,
+                pipelineLayout,
+                VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+                0,
+                sizeof(SimplePushConstantData),
+                &push);
+            obj.model->bind(commandBuffer);
+            obj.model->draw(commandBuffer);
+        }
+    }
+}
+
+
+/* OLD COMMANDBUFFERS
+        commandBuffers.resize(lveSwapChain->imageCount());
+
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = lveDevice.getCommandPool();
+        allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+        if (vkAllocateCommandBuffers(lveDevice.device(), &allocInfo, commandBuffers.data()) !=
+            VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate command buffers!");
+        }
+
+        for (int i = 0; i < commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+                throw std::runtime_error("failed to begin recording command buffer!");
+            }
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = lveSwapChain->getRenderPass();
+            renderPassInfo.framebuffer = lveSwapChain->getFrameBuffer(i);
+
+            renderPassInfo.renderArea.offset = { 0, 0 };
+            renderPassInfo.renderArea.extent = lveSwapChain->getSwapChainExtent();
+
+            std::array<VkClearValue, 2> clearValues{};
+            clearValues[0].color = { 0.1f, 0.1f, 0.1f, 1.0f };
+            clearValues[1].depthStencil = { 1.0f, 0 };
+            renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+            renderPassInfo.pClearValues = clearValues.data();
+
+            vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+            lvePipeline->bind(commandBuffers[i]);
+            lveModel->bind(commandBuffers[i]);
+            lveModel->draw(commandBuffers[i]);
+
+            vkCmdEndRenderPass(commandBuffers[i]);
+            if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to record command buffer!");
+            }
+        }
+        */
+
+        /*
+                void SimpleRenderSystem::sierpinski(
+                    std::vector<LveModel::Vertex>& vertices,
+                    int depth,
+                    glm::vec2 left,
+                    glm::vec2 right,
+                    glm::vec2 top) {
+                    if (depth <= 0) {
+                        vertices.push_back({ top });
+                        vertices.push_back({ right });
+                        vertices.push_back({ left });
+                    }
+                    else {
+                        auto leftTop = 0.5f * (left + top);
+                        auto rightTop = 0.5f * (right + top);
+                        auto leftRight = 0.5f * (left + right);
+                        sierpinski(vertices, depth - 1, left, leftRight, leftTop);
+                        sierpinski(vertices, depth - 1, leftRight, right, rightTop);
+                        sierpinski(vertices, depth - 1, leftTop, rightTop, top);
+                    }
+                }
+                */
